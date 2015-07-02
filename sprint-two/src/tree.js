5513ed38fb1c1b20c3a7c509575fd50cfea60a8e
MKS-students <mks.students@hackreactor.com>
2015-07-02 12:29:53 -0700

    implements tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,24 +2,35 @@ var Tree = function(value){
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 
 
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var childTree = Tree(value);
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target){
-
+  var found = false;
+  var recurse = function(tree) {
+    if(tree.value === target) {
+      found = true;
+    }
+    if(tree.children.length) {
+      for(var i = 0; i < tree.children.length; i++) {
+        recurse(tree.children[i]);
+      }
+    }
+    return found;
+  };
+  return recurse(this);
 };
 
 
