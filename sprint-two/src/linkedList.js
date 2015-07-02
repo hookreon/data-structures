8fa423600c1b1ba6e288df1503c4c0cd48148926
MKS-students <mks.students@hackreactor.com>
2015-07-01 21:45:32 -0700

    implements linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+
+    if(!list.head) {
+      list.head = newNode;
+    }
+    if ( list.tail !== null ) {
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var temp = list.head.value;
+    list.head = list.head.next;
+    return temp;
   };
 
   list.contains = function(target){
+    var recurse = function(node) {
+
+      if(node.value === target) {
+        return true;
+      }
+      if(node.next === null) {
+        return false;
+      }
+      return recurse(node.next);
+    };
+    return recurse(list.head);
   };
 
   return list;
