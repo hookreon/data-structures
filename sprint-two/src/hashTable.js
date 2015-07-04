843769e77cba9c1638b4b8c2206929d9333b5a2e
MKS-students <mks.students@hackreactor.com>
2015-07-03 20:13:17 -0700

    implements hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,18 +5,46 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+
+  if (!bucket) {
+    bucket = [];
+  } else {
+    for (var j = 0; j < bucket.length; j++) {
+      if (bucket[j][0] === k) {
+        bucket[j][1] = v;
+      }
+    }
+  }
+  bucket.push([k,v]);
+  this._storage.set(i, bucket);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
 
+  if(bucket) {
+    for(var j = 0; j < bucket.length;j++) {
+      if(bucket[j][0] === k) {
+        return bucket[j][1];
+      }
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
 
+  if(bucket) {
+    bucket = _.reject(bucket, function(kvp){
+      return kvp[0] === k;
+    });
+  }
+  this._storage.set(i, bucket);
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
